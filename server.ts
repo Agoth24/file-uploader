88ce0090180483574583d8d6528bf21308ad1668
Agoth Arop <agoth.a3@gmail.com>
2026-05-03 11:59:05 -0600

    server and better-auth setup

---
+++ server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./utils/auth";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.all("/api/auth/*", toNodeHandler(auth));
+app.use(express.json())
+
+
+
+
+
+
+app.listen(PORT, () => {
+	console.log(`Listening on port ${PORT}`);
+	console.log(`Process ID: ${process.pid}`);
+});
