bafba33daa09ef07cf30828be8be61a77eae7afb
Agoth Arop <agoth.a3@gmail.com>
2026-05-09 00:50:43 -0600

    rename & reparameterize file services

---
+++ services/fileServices.ts
@@ -0,0 +1,27 @@
+import type { File } from "../generated/prisma/client";
+import { Prisma } from "../generated/prisma/client";
+
+
+const getFiles = async () => {
+        
+}
+
+const getFileById = async (id: string) => {
+    
+}
+
+const downloadFileById = async (id: string) => {
+    
+}
+
+const uploadFile = async (file: File) => {
+    
+}
+
+const updateFileById = async (id: string, data: Partial<File>) => {
+
+}
+
+const deleteFileById = async (id: string) => {
+    
+}
